e88f14c5b76223e400ae5cdf29016ae21aa3ceb0
Senne Vande Sompele <senne.vande.sompele@geoit.be>
2020-06-25 10:32:08 +0200

    #38

--- src/system_sensors.py
+++ src/system_sensors.py
@@ -12,7 +12,11 @@ import pytz
 import yaml
 from pytz import timezone
 import argparse
-
+try:
+    import apt
+    apt_disabled = False
+except ImportError:
+    apt_disabled = True
 UTC = pytz.utc
 DEFAULT_TIME_ZONE = None
 
@@ -20,6 +24,7 @@ mqttClient = None
 SYSFILE = "/sys/devices/platform/soc/soc:firmware/get_throttled"
 WAIT_TIME_SECONDS = 60
 deviceName = None
+
 class ProgramKilled(Exception):
     pass
 
@@ -65,32 +70,46 @@ def as_local(dattim: dt.datetime) -> dt.datetime:
 def get_last_boot():
     return str(as_local(utc_from_timestamp(psutil.boot_time())).isoformat())
 
+def on_message(client, userdata, message):
+    print (f"Message received: {message.payload.decode()}"  )
+    if(message.payload.decode() == "online"):
+        send_config_message(client)
+    
 
 def updateSensors():
-    payload_str=('{"temperature": '
-    + get_temp()
-    + ', "disk_use": '
-    + get_disk_usage("/")
-    + ', "memory_use": '
-    + get_memory_usage()
-    + ', "cpu_usage": '
-    + get_cpu_usage()
-    + ', "swap_usage": '
-    + get_swap_usage()
-    + ', "power_status": "'
-    + get_rpi_power_status()
-    + '", "last_boot": "'
-    + get_last_boot()
-    + '", "last_message": "'
-    + time.ctime()+'"')
-    if "check_available_updates" in settings and settings["check_available_updates"]:
+    payload_str = (
+        '{"temperature": '
+        + get_temp()
+        + ', "disk_use": '
+        + get_disk_usage("/")
+        + ', "memory_use": '
+        + get_memory_usage()
+        + ', "cpu_usage": '
+        + get_cpu_usage()
+        + ', "swap_usage": '
+        + get_swap_usage()
+        + ', "power_status": "'
+        + get_rpi_power_status()
+        + '", "last_boot": "'
+        + get_last_boot()
+        + '", "last_message": "'
+        + time.ctime()
+        + '"'
+    )
+    if "check_available_updates" in settings and settings["check_available_updates"] and not apt_disabled:
         payload_str = payload_str + ', "updates": ' + get_updates()
     if "check_wifi_strength" in settings and settings["check_wifi_strength"]:
         payload_str = payload_str + ', "wifi_strength": ' + get_wifi_strength()
     if "external_drives" in settings:
         for drive in settings["external_drives"]:
-            payload_str = payload_str + ', "disk_use_'+ drive.lower() +'": ' + get_disk_usage(settings["external_drives"][drive])
-    payload_str = payload_str + '}'
+            payload_str = (
+                payload_str
+                + ', "disk_use_'
+                + drive.lower()
+                + '": '
+                + get_disk_usage(settings["external_drives"][drive])
+            )
+    payload_str = payload_str + "}"
     mqttClient.publish(
         topic="system-sensors/sensor/" + deviceName + "/state",
         payload=payload_str,
@@ -98,11 +117,13 @@ def updateSensors():
         retain=False,
     )
 
+
 def get_updates():
-    cache=apt.Cache()
+    cache = apt.Cache()
     cache.open(None)
     cache.upgrade()
     return str(cache.get_changes().__len__())
+
 
 def get_temp():
     temp = check_output(["vcgencmd", "measure_temp"]).decode("UTF-8")
@@ -124,8 +145,20 @@ def get_cpu_usage():
 def get_swap_usage():
     return str(psutil.swap_memory().percent)
 
-def get_wifi_strength():#check_output(["/proc/net/wireless", "grep wlan0"])
-     return check_output(['bash', '-c', "cat /proc/net/wireless | grep wlan0: | awk '{print int($4)}'"]).decode('utf-8').rstrip()
+
+def get_wifi_strength():  # check_output(["/proc/net/wireless", "grep wlan0"])
+    return (
+        check_output(
+            [
+                "bash",
+                "-c",
+                "cat /proc/net/wireless | grep wlan0: | awk '{print int($4)}'",
+            ]
+        )
+        .decode("utf-8")
+        .rstrip()
+    )
+
 
 def get_rpi_power_status():
     _throttled = open(SYSFILE, "r").read()[:-1]
@@ -151,6 +184,7 @@ def get_rpi_power_status():
         else:
             return "There is a problem with your power supply or system."
 
+
 def check_settings(settings):
     if "mqtt" not in settings:
         print("Mqtt not defined in settings.yaml! Please check the documentation")
@@ -173,33 +207,9 @@ def check_settings(settings):
         sys.stdout.flush()
         sys.exit()
 
-def _parser():
-    """Generate argument parser"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("settings", help="path to the settings file")
-    return parser
 
-if __name__ == "__main__":
-    args = _parser().parse_args()
-    with open(args.settings) as f:
-        # use safe_load instead load
-        settings = yaml.safe_load(f)
-    check_settings(settings)
-    DEFAULT_TIME_ZONE = timezone(settings["timezone"])
-    if "update_interval" in settings:
-        WAIT_TIME_SECONDS = settings["update_interval"]
-    mqttClient = mqtt.Client(client_id=settings["client_id"])
-    deviceName = settings["deviceName"]
-    if "user" in settings["mqtt"]:
-        mqttClient.username_pw_set(
-            settings["mqtt"]["user"], settings["mqtt"]["password"]
-        )  # Username and pass if configured otherwise you should comment out this
-    signal.signal(signal.SIGTERM, signal_handler)
-    signal.signal(signal.SIGINT, signal_handler)
-    if "port" in settings["mqtt"]:
-        mqttClient.connect(settings["mqtt"]["hostname"], settings["mqtt"]["port"])
-    else:
-        mqttClient.connect(settings["mqtt"]["hostname"], 1883)
+def send_config_message(mqttClient):
+    print("send config message")
     mqttClient.publish(
         topic="homeassistant/sensor/" + deviceName + "/" + deviceName + "Temp/config",
         payload='{"device_class":"temperature","name":"'
@@ -350,7 +360,7 @@ if __name__ == "__main__":
         qos=1,
         retain=True,
     )
-    
+
     mqttClient.publish(
         topic="homeassistant/sensor/"
         + deviceName
@@ -375,29 +385,32 @@ if __name__ == "__main__":
     )
 
     if "check_available_updates" in settings and settings["check_available_updates"]:
-        import apt
-        mqttClient.publish(
-            topic="homeassistant/sensor/"
-            + deviceName
-            + "/"
-            + deviceName
-            + "Updates/config",
-            payload='{"name":"'
-            + deviceName
-            + 'Updates","state_topic":"system-sensors/sensor/'
-            + deviceName
-            + '/state","value_template":"{{ value_json.updates}}","unique_id":"'
-            + deviceName.lower()
-            + '_sensor_updates","device":{"identifiers":["'
-            + deviceName.lower()
-            + '_sensor"],"name":"'
-            + deviceName
-            + 'Sensors","model":"RPI '
-            + deviceName
-            + '","manufacturer":"RPI"}, "icon":"mdi:cellphone-arrow-down"}',
-            qos=1,
-            retain=True,
-        )
+        # import apt
+        if(apt_disabled):
+            print("import of apt failed!")
+        else:
+            mqttClient.publish(
+                topic="homeassistant/sensor/"
+                + deviceName
+                + "/"
+                + deviceName
+                + "Updates/config",
+                payload='{"name":"'
+                + deviceName
+                + 'Updates","state_topic":"system-sensors/sensor/'
+                + deviceName
+                + '/state","value_template":"{{ value_json.updates}}","unique_id":"'
+                + deviceName.lower()
+                + '_sensor_updates","device":{"identifiers":["'
+                + deviceName.lower()
+                + '_sensor"],"name":"'
+                + deviceName
+                + 'Sensors","model":"RPI '
+                + deviceName
+                + '","manufacturer":"RPI"}, "icon":"mdi:cellphone-arrow-down"}',
+                qos=1,
+                retain=True,
+            )
 
     if "check_wifi_strength" in settings and settings["check_wifi_strength"]:
         mqttClient.publish(
@@ -429,14 +442,22 @@ if __name__ == "__main__":
                 + deviceName
                 + "/"
                 + deviceName
-                + "DiskUse"+ drive +"/config",
+                + "DiskUse"
+                + drive
+                + "/config",
                 payload='{"name":"'
                 + deviceName
-                + 'DiskUse'+ drive +'","state_topic":"system-sensors/sensor/'
+                + "DiskUse"
+                + drive
+                + '","state_topic":"system-sensors/sensor/'
                 + deviceName
-                + '/state","unit_of_measurement":"%","value_template":"{{ value_json.disk_use_'+ drive.lower() +'}}","unique_id":"'
+                + '/state","unit_of_measurement":"%","value_template":"{{ value_json.disk_use_'
+                + drive.lower()
+                + '}}","unique_id":"'
                 + deviceName.lower()
-                + '_sensor_disk_use_'+ drive.lower() +'","device":{"identifiers":["'
+                + "_sensor_disk_use_"
+                + drive.lower()
+                + '","device":{"identifiers":["'
                 + deviceName.lower()
                 + '_sensor"],"name":"'
                 + deviceName
@@ -446,15 +467,59 @@ if __name__ == "__main__":
                 qos=1,
                 retain=True,
             )
+
+def _parser():
+    """Generate argument parser"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("settings", help="path to the settings file")
+    return parser
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to broker")
+        client.subscribe("hass/status")
+    else: 
+        print("Connection failed")
+
+if __name__ == "__main__":
+    args = _parser().parse_args()
+    with open(args.settings) as f:
+        # use safe_load instead load
+        settings = yaml.safe_load(f)
+    check_settings(settings)
+    DEFAULT_TIME_ZONE = timezone(settings["timezone"])
+    if "update_interval" in settings:
+        WAIT_TIME_SECONDS = settings["update_interval"]
+    mqttClient = mqtt.Client(client_id=settings["client_id"])
+    mqttClient.on_connect = on_connect                      #attach function to callback
+    mqttClient.on_message = on_message
+    deviceName = settings["deviceName"]
+    if "user" in settings["mqtt"]:
+        mqttClient.username_pw_set(
+            settings["mqtt"]["user"], settings["mqtt"]["password"]
+        )  # Username and pass if configured otherwise you should comment out this
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
+    if "port" in settings["mqtt"]:
+        mqttClient.connect(settings["mqtt"]["hostname"], settings["mqtt"]["port"])
+    else:
+        mqttClient.connect(settings["mqtt"]["hostname"], 1883)
+    try:
+        send_config_message(mqttClient)
+    except:
+        print("something whent wrong")
     job = Job(interval=timedelta(seconds=WAIT_TIME_SECONDS), execute=updateSensors)
     job.start()
-    # mqttClient.loop_forever()
+    
     mqttClient.loop_start()
+    
     while True:
-        try:            
+        try:
+            sys.stdout.flush()
             time.sleep(1)
         except ProgramKilled:
             print("Program killed: running cleanup code")
+            mqttClient.disconnect()
             mqttClient.loop_stop()
             sys.stdout.flush()
             job.stop()
