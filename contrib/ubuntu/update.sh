2f01bb05cf759046f6aa4894db8ab1d17533b6ba
Ben Mepham <25695617+benmepham@users.noreply.github.com>
2023-10-31 15:31:25 +0000

    clean up update script

--- contrib/ubuntu/update.sh
+++ contrib/ubuntu/update.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 set -e
 
+echo "Stopping system_sensors"
+systemctl stop system_sensors
+
 echo "Update and install any new dependencies"
 apt update
 apt install -y curl python3 python3-pip python3-dev python3-apt
 
-
 echo "Activating venv"
 source /opt/systemsensors/venv/bin/activate
-which python
-echo "Install any new pip requirements"
 
+echo "Install any new pip requirements"
 curl -o /tmp/requirements.txt https://raw.githubusercontent.com/benmepham/system_sensors/master/requirements.txt
 pip3 install -r /tmp/requirements.txt
 
+echo "Updating system_sensors"
 curl -o /opt/systemsensors/sensors.py https://raw.githubusercontent.com/benmepham/system_sensors/master/src/sensors.py
 curl -o /opt/systemsensors/system_sensors.py  https://raw.githubusercontent.com/benmepham/system_sensors/master/src/system_sensors.py
 
 chmod 770 -R /opt/systemsensors
 chown -R systemsensors:systemsensors /opt/systemsensors
 
+echo "Starting system_sensors"
+systemctl start system_sensors
 
-systemctl restart system_sensors
-systemctl status system_sensors
-
+echo "Done, see the status with 'systemctl status system_sensors'"
