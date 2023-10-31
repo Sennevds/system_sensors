bb4e8e0e1060a2f4c0567b05284be9a43a239063
Ben Mepham <25695617+benmepham@users.noreply.github.com>
2023-10-31 14:56:41 +0000

    add update script

---
+++ contrib/ubuntu/update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "update and install any new deps"
+apt update
+apt install -y curl python3 python3-pip python3-dev python3-apt
+
+echo "Install any new pip requirements"
+
+curl -o /tmp/requirements.txt https://raw.githubusercontent.com/benmepham/system_sensors/master/requirements.txt
+pip3 install -r /tmp/requirements.txt
+
+curl -o /opt/systemsensors/sensors.py https://raw.githubusercontent.com/benmepham/system_sensors/master/src/sensors.py
+curl -o /opt/systemsensors/system_sensors.py  https://raw.githubusercontent.com/benmepham/system_sensors/master/src/system_sensors.py
+
+chmod 770 -R /opt/systemsensors
+chown -R systemsensors:systemsensors /opt/systemsensors
+
+
+systemctl restart system_sensors
+systemctl status system_sensors
+
